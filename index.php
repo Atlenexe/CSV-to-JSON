df47de8243c366ad549771faccb4dc8d14acea0e
Atlenexe <hugo.mandou@gmail.com>
2023-11-16 14:25:39 +0100

    Création formulaire

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style.css">
-    <title>Eval PHP</title>
+    <title>Eval PHP: CSV to JSON</title>
 </head>
 
 <body>
     <main>
-
+        <form action="" method="post">
+            <input type="file" name="file" id="file">
+            <input type="submit" value="Convertir le fichier en JSON">
+        </form>
     </main>
 </body>
 
